968e88abc9732da5b06efea79099db556fb3630f
samorenoq <samorenoq@eafit.edu.co>
2020-05-27 16:23:14 -0500

    Más cambios

--- temperature/views.py
+++ temperature/views.py
@@ -12,13 +12,13 @@ def temperature(request):
         if value and latitud and longitud and tipo_terreno:
             # Crea el json para realizar la petición POST al Web Service
             args = {'type': '°C', 'value': value, 'latitud':latitud, 'longitud': longitud, 'tipo_terreno': tipo_terreno}
-            response = requests.post('http://127.0.0.1:8000/temperature/', args)
+            #response = requests.post('http://127.0.0.1:8000/temperature/', args)
             response = requests.post('http://pi1-eafit-samorenoq.azurewebsites.net/temperature/', args)
             # Convierte la respuesta en JSON
             temperature_json = response.json()
 
     # Realiza una petición GET al Web Services
-    response = requests.get('http://127.0.0.1:8000/temperature/')
+    #response = requests.get('http://127.0.0.1:8000/temperature/')
     response = requests.get('http://pi1-eafit-samorenoq.azurewebsites.net/temperature/')
     # Convierte la respuesta en JSON
     temperatures = response.json()
